35fd77c22eee7edb6599ecf4bf9fa8d27084a244
SarahAsliPolat <asliatamanpolat@gmail.com>
2021-03-13 13:46:08 -0500

    branchPractice

--- src/main/java/featureACode.java
+++ src/main/java/featureACode.java
@@ -1,3 +1,8 @@
 public class featureACode {
 
+    public static void main(String[] args) {
+
+        System.out.println("branch practice");
+        System.out.println("learning git in details");
+    }
 }
