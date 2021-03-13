4f97083da34bc6a69ac20d9f12bee11b2fef982a
SarahAsliPolat <asliatamanpolat@gmail.com>
2021-03-13 12:15:55 -0500

    first commit

--- src/main/java/GitPractice.java
+++ src/main/java/GitPractice.java
@@ -2,5 +2,7 @@ public class GitPractice {
     public static void main(String[] args) {
 
         System.out.println("Hello World after a long time");
+
+        System.out.println("some chnages to observe");
     }
 }
